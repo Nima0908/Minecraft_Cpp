1c22c819188ad96d1a18e0912a42ed10bb8df2ce
Nima0908 <marius.webert@web.de>
2025-07-09 23:01:25 +0200

    Vulkan test progress

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <stdexcept>
 #include <vector>
 #include <vulkan/vulkan_core.h>
+#include <optional>
 
 #ifdef NDEBUG
 const bool enableValidationLayers = false;
@@ -112,6 +113,38 @@ private:
   void initVulkan() {
     createInstance();
     setupDebugMessenger();
+    pickPhysicalDevice();
+  }
+
+  void pickPhysicalDevice() {
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+    if (deviceCount == 0) {
+    throw std::runtime_error("failed to find GPUs with Vulkan support!");
+    }
+    
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    for (const auto& device : devices) {
+        if (isDeviceSuitable(device)) {
+            physicalDevice = device;
+            break;
+        }
+    }
+
+    if (physicalDevice == VK_NULL_HANDLE) {
+        throw std::runtime_error("failed to find a suitable GPU!");
+      }
+
+  }
+
+  bool isDeviceSuitable(VkPhysicalDevice device) {
+    QueueFamilyIndices indices = findQueueFamilies(device);
+
+    return indices.graphicsFamily.has_value();
   }
 
   void populateDebugMessengerCreateInfo(
@@ -215,6 +248,39 @@ private:
     }
 
     return extensions;
+  }
+
+  struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+
+    bool isComplete() {
+        return graphicsFamily.has_value();
+    }
+  };
+
+  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+      QueueFamilyIndices indices;
+      // Logic to find queue family indices to populate struct with
+      uint32_t queueFamilyCount = 0;
+      vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+      std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+      vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+      
+      int i = 0;
+      for (const auto& queueFamily : queueFamilies) {
+          if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+              indices.graphicsFamily = i;
+          }
+
+          if (indices.isComplete()) {
+            break;
+          }
+
+          i++;
+      }
+
+      return indices;
   }
 
   static VKAPI_ATTR VkBool32 VKAPI_CALL
